63bbb2e744b6dbe3c2daf1625b4932e6fc84723b
Nathan <nathanamar19@gmail.com>
2018-10-21 23:32:58 +0200

    about php

---
+++ about.php
@@ -0,0 +1,270 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Jobscoring </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet"> -->
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+			<?php include 'menu.php' ?>
+		</div>
+		<!-- end:fh5co-header -->
+		<aside id="fh5co-hero" class="js-fullheight">
+			<div class="flexslider js-fullheight">
+				<ul class="slides">
+			   	<li style="background-image: url(images/kahol.png);">
+			   		<div class="overlay-gradient"></div>
+			   		<div class="container">
+			   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner desc">
+			   					<h2 class="heading-section">Notre Métier</h2>
+									<i><h2 class="heading-section">Votre Carrière</h2><i/>
+
+			   				</div>
+			   			</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+
+		<div id="fh5co-services-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+						<i class="sl-icon-paper-plane"></i>
+						<h2>Nos services</h2>
+						<p>JobScoring est une plateforme mettant en contact entreprises et candidats sous une approche totalement anonyme </p>
+					</div>
+				</div>
+				<div class="row">
+									<div class="col-md-4 text-center">
+										<div class="services">
+											<span><i class="sl-icon-people"></i></span>
+											<h3>Équité a l’embauche</h3>
+											<p>Afin de vous aider à trouver votre job, nous avons construit un algorithme capable d’établir un pourcentage de compatibilité entre vos compétences et un job</p>
+										</div>
+									</div>
+
+									<div class="col-md-4 text-center">
+										<div class="services">
+											<span><i class="sl-icon-calculator"></i></span>
+											<h3>Algorithme de compatibilité</h3>
+											<p>Ce pourcentage de compatibilité sera établi uniquement sur vos compétences</p>
+										</div>
+									</div>
+									<div class="col-md-4 text-center">
+										<div class="services">
+											<span><i class="sl-icon-eye"></i></span>
+											<h3>Anonymat</h3>
+											<p>Cet anonymat vous promet une équité à l’embauche! Seul vos compétences entre en compte</p>
+										</div>
+									</div>
+
+									<div id="fh5co-about-section">
+										<div class="container">
+											<div class="row">
+												<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+													<i class="sl-icon-people"></i>
+													<h2>Notre équipe</h2>
+													<br/>
+													<br/>
+												</div>
+											</div>
+											<div class="row">
+												<div class="col-md-4 text-center">
+													<div class="about-wrap">
+														<div class="about">
+															<img src="images/nathan1.png" alt="">
+														</div>
+														<div class="desc">
+															<h3>Nathan Amar</h3>
+														<!--	<p class="pos">CEO, Developer</p> -->
+															<p class="fh5co-social-icons">
+																<a href="#"><i class="icon-twitter2"></i></a>
+																<a href="#"><i class="icon-facebook2"></i></a>
+																<a href="#"><i class="icon-instagram"></i></a>
+																<a href="#"><i class="icon-dribbble2"></i></a>
+																<a href="#"><i class="icon-youtube"></i></a>
+															</p>
+														</div>
+													</div>
+												</div>
+
+
+												<div class="col-md-4 text-center">
+													<div class="about-wrap">
+														<div class="about">
+															<img src="images/corentin1.png" alt="">
+														</div>
+														<div class="desc">
+															<h3>Corentin Bidault</h3>
+															<p class="fh5co-social-icons">
+																<a href="#"><i class="icon-twitter2"></i></a>
+																<a href="#"><i class="icon-facebook2"></i></a>
+																<a href="#"><i class="icon-instagram"></i></a>
+																<a href="#"><i class="icon-dribbble2"></i></a>
+																<a href="#"><i class="icon-youtube"></i></a>
+															</p>
+														</div>
+													</div>
+												</div>
+												<div class="col-md-4 text-center">
+													<div class="about-wrap">
+														<div class="about">
+															<img src="images/alexandre.png" alt="">
+														</div>
+														<div class="desc">
+															<h3>Alexandre Benezech</h3>
+															<p class="fh5co-social-icons">
+																<a href="#"><i class="icon-twitter2"></i></a>
+																<a href="#"><i class="icon-facebook2"></i></a>
+																<a href="#"><i class="icon-instagram"></i></a>
+																<a href="#"><i class="icon-dribbble2"></i></a>
+																<a href="#"><i class="icon-youtube"></i></a>
+															</p>
+														</div>
+													</div>
+												</div>
+											</div>
+
+
+											<div class="row">
+												<div class="col-md-4 text-center">
+													<div class="about-wrap">
+														<div class="about">
+															<img src="images/ewen.png" alt="">
+														</div>
+														<div class="desc">
+															<h3>Ewenn Battas</h3>
+														<!--	<p class="pos">Developer Web</p>-->
+															<p class="fh5co-social-icons">
+																<a href="#"><i class="icon-twitter2"></i></a>
+																<a href="#"><i class="icon-facebook2"></i></a>
+																<a href="#"><i class="icon-instagram"></i></a>
+																<a href="#"><i class="icon-dribbble2"></i></a>
+																<a href="#"><i class="icon-youtube"></i></a>
+															</p>
+														</div>
+													</div>
+
+
+
+
+
+										</div>
+
+
+
+
+														<div class="row">
+															<div class="col-md-4 text-center">
+																<div class="about-wrap">
+																	<div class="about">
+																		<img src="images/aymeric.png" alt="">
+																	</div>
+																	<div class="desc">
+																		<h3>Aymeric Vagaggini-Bieysse</h3>
+																		<p class="pos">Developer Web</p>
+																		<p class="fh5co-social-icons">
+																			<a href="#"><i class="icon-twitter2"></i></a>
+																			<a href="#"><i class="icon-facebook2"></i></a>
+																			<a href="#"><i class="icon-instagram"></i></a>
+																			<a href="#"><i class="icon-dribbble2"></i></a>
+																			<a href="#"><i class="icon-youtube"></i></a>
+																		</p>
+																	</div>
+																</div>
+
+
+
+
+
+																		</div>
+
+
+		<?php include 'footer.php' ?>
+
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- jQuery -->
+
+
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Superfish -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
