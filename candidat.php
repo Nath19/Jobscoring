52464791c38b7134fa3a6dade05f174bbde08837
Nathan <nathanamar19@gmail.com>
2018-10-21 23:34:48 +0200

    Candidat php

---
+++ candidat.php
@@ -0,0 +1,277 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Energy &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet"> -->
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+		<?php include 'menu.php' ?>
+
+		<aside id="fh5co-hero" class="js-fullheight">
+			<div class="flexslider js-fullheight">
+				<ul class="slides">
+			   	<li style="background-image: url(images/slide_3.jpg);">
+			   		<div class="overlay-gradient"></div>
+			   		<div class="container">
+			   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+			   				<div class="slider-text-inner desc">
+			   					<h2 class="heading-section">Changement de carrière ?  </h2>
+									 <h2 class="heading-section">   Mission accomplie ! </h2>
+			   				</div>
+			   			</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+		<div id="fh5co-blog-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+						<i class="sl-icon-note"></i>
+						<h2>Inscrivez-vous, remplissez votre CV électronique et JobScoring vous trouve votre métier qui vous correspond !</h2>
+						<p>Des solutions pour tout type de contrat professionnel</p>
+
+					<i><p>CDI,CDD,STAGE,INTERIM</p><i/>
+						<p><a href="sign_up.php" class="btn btn-primary btn-lg">Déposer votre CV </a></p>
+
+					</div>
+				</div>
+
+			<!--	<div class="container-fluid proj-bottom">
+			<div class="row">
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/webmaster.png" alt="" class="img-responsive">
+						<h3>Web Master</h3>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/Programming.png" alt="" class="img-responsive">
+						<h3>Programming</h3>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/Photography.png" alt="" class="img-responsive">
+						<h3>Photography</h3>
+					</a>
+				</div>
+				<div class="row">
+					<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+						<a href="#"><img src="images/webmaster.png" alt="" class="img-responsive">
+							<h3>Web Master</h3>
+						</a>
+					</div>
+					<div class="row">
+						<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+							<a href="#"><img src="images/webmaster.png" alt="" class="img-responsive">
+								<h3>Web Master</h3>
+							</a>
+						</div>
+						<div class="row">
+							<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+								<a href="#"><img src="images/webmaster.png" alt="" class="img-responsive">
+									<h3>Web Master</h3>
+								</a>
+							</div>
+							<div class="row">
+								<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+									<a href="#"><img src="images/webmaster.png" alt="" class="img-responsive">
+										<h3>Web Master</h3>
+									</a>
+								</div>
+					</div>
+				</div>
+			</div>
+			-->
+
+
+				<!-- END #fh5co-header -->
+				<div class="container-fluid">
+					<div class="row fh5co-post-entry">
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/webmaster.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<h2 class="fh5co-article-title"><a/>Webmaster</a></h2>
+						</article>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/finance.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<span class="fh5co-meta"><a href="single.html"></a></span>
+							<h2 class="fh5co-article-title"><a href="single.html">Finance</a></h2>
+						</article>
+						<div class="clearfix visible-xs-block"></div>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/programming.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<span class="fh5co-meta"><a href="single.html"></a><a/></a></span>
+							<h2 class="fh5co-article-title"><a href="single.html">Programming</a></h2>
+							<span class="fh5co-meta fh5co-date"></span>
+						</article>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/aero.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<h2 class="fh5co-article-title"><a />Aéronautique</a></h2>
+						</article>
+						<div class="clearfix visible-lg-block visible-md-block visible-sm-block visible-xs-block"></div>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a/><img src="images/photography.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<h2 class="fh5co-article-title"><a/>Photographie</a></h2>
+						</article>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/commerce.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<h2 class="fh5co-article-title"><a href="single.html">Commerce</a></h2>
+						</article>
+						<div class="clearfix visible-xs-block"></div>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/hotelerie.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<h2 class="fh5co-article-title"><a/>Hôtellerie</a></h2>
+						</article>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/biologie.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<h2 class="fh5co-article-title"><a />Biologie</a></h2>
+						</article>
+						<div class="clearfix visible-lg-block visible-md-block visible-sm-block visible-xs-block"></div>
+
+
+
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a/><img src="images/avocat.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<span class="fh5co-meta"><a href="single.html"></a></span>
+							<h2 class="fh5co-article-title"><a href="single.html">Droit</a></h2>
+						</article>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/sante.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<span class="fh5co-meta"><a href="single.html"></a></span>
+							<h2 class="fh5co-article-title"><a href="single.html">Santé</a></h2>
+							<span class="fh5co-meta fh5co-date"></span>
+						</article>
+						<div class="clearfix visible-xs-block"></div>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a/><img src="images/journalisme.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<span class="fh5co-meta"><a href="single.html"></a><a href="single.html"></a></span>
+							<h2 class="fh5co-article-title"><a href="single.html">Journalisme</a></h2>
+						</article>
+						<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
+							<figure>
+								<a /><img src="images/immobilier.png" alt="Image" class="img-responsive"></a>
+							</figure>
+							<span class="fh5co-meta"><a href="single.html"></a> <a href="single.html"></a></span>
+							<h2 class="fh5co-article-title"><a href="single.html">Immobilier</a></h2>
+						</article>
+
+
+					</div>
+				</div>
+
+
+
+
+
+
+		</div>
+
+
+
+
+		<?php include 'footer.php' ?>
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- jQuery -->
+
+
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Superfish -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
