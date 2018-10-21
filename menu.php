c3273e0d3fb11f900d16b04926cf8145d5033397
Nathan <nathanamar19@gmail.com>
2018-10-21 23:48:25 +0200

    Menu

---
+++ menu.php
@@ -0,0 +1,35 @@
+<div id="fh5co-wrapper">
+<div id="fh5co-page">
+<div id="fh5co-header">
+  <header id="fh5co-header-section">
+    <div class="container">
+      <div class="nav-header">
+        <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+        <h1 id="fh5co-logo"><a href="index.php">Jobscoring</a></h1>
+        <!-- START #fh5co-menu-wrap -->
+        <nav id="fh5co-menu-wrap" role="navigation">
+          <ul class="sf-menu" id="fh5co-primary-menu">
+          <!-- 	<li>
+              <a class="active" href="index.html">Home</a>
+            </li> -->
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="candidat.php">Candidats</a></li>
+            <li><a href="employeur.php">Employeurs</a></li>
+            <li>
+          									<a href="about.php" class="fh5co-sub-ddown">A Propos</a>
+          									 <ul class="fh5co-sub-menu">
+          										<li><a href="contact.php">Contact</a></li>
+                            </ul>
+          										<li>
+            <li><a href="#">Mon compte</a>
+              <ul class="fh5co-sub-menu">
+                  <li><a href="sign_up.php">S'inscrire</a></li>
+                  <li><a href="login.php">S'identifier</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</div>
