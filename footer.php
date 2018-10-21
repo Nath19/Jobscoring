f33f79a1e4c8e4b2dfd3751dd076ba9e824cf653
Nathan <nathanamar19@gmail.com>
2018-10-21 23:41:49 +0200

    Footer

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="copyright">
+          <p><small>&copy; 2018  JobScoring<a href="index.html"></a><br> All Rights Reserved. <br>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-4">
+            <h3>JobScoring</h3>
+            <ul class="link">
+              <li><a href="about.php">A propos</a></li>
+              <li><a href="contact.php">Nous contacter</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <h3>Notre objectif ?</h3>
+            <p>Recentrer le monde de l'emploi sur ce qui est essentiel : les compétences</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <ul class="social-icons">
+          <li>
+            <a href="#"><i class="icon-twitter-with-circle"></i></a>
+            <a href="#"><i class="icon-facebook-with-circle"></i></a>
+            <a href="#"><i class="icon-instagram-with-circle"></i></a>
+            <a href="#"><i class="icon-linkedin-with-circle"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
